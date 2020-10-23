c7878213673326671c0ff91701a60cccbaa08ba2
Bumduck96 <bs961212@gmail.com>
2020-10-23 14:50:29 +0900

    update

--- js/index_event.js
+++ js/index_event.js
@@ -6,4 +6,13 @@ $(function(){
         //location.href="#"+$(this).text();
         $('html, body').animate({scrollTop : $("#"+$(this).text()).offset().top}, 700);
     });
+    $("#memu_Box").on("click",function(){
+        if($("#menu_Bar1").hasClass("on")){
+            $(".menu_Bar").removeClass("on");
+            $('#header_ ul').slideUp(300);
+        }else{
+            $(".menu_Bar").addClass("on");
+            $('#header_ ul').slideDown(300);
+        }
+    });
 });
